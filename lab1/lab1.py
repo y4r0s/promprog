031347d24564175e7e4abade4cff70fcd1bf4552
Yaros <conkurentik2.gmail.com>
2026-05-17 23:59:34 +0300

    Initial commit: binary search base

---
+++ lab1/lab1.py
@@ -0,0 +1,22 @@
+def binary_search(arr, target):
+    low = 0
+    high = len(arr)-1
+    mid = (low+high)//2
+    guess = arr[mid]
+
+    while low<=high:
+        if guess == target:
+            return mid
+        elif guess>target:
+            high=mid-1
+
+        else:
+            low = mid+1
+
+    return -1
+
+
+my_list = [1,3,5,7,9,11,13,15]
+target_value=7
+
+result = binary_search(my_list,target_value)
