d452f61bf080281e5911a137d8969d58994c6384
Yaros <conkurentik2.gmail.com>
2026-05-18 00:21:06 +0300

    docs: docstring added

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,17 +1,26 @@
 def binary_search(arr: list, target: int) -> int:
-    low = 0
-    high = len(arr)-1
-    mid = (low+high)//2
-    guess = arr[mid]
+    """Выполняет бинарный поиск элемента в отсортированном массиве.
 
-    while low<=high:
+    Args:
+        arr (list): Отсортированный список элементов для поиска.
+        target (int): Искомое значение.
+
+    Returns:
+        int: Индекс найденного элемента в массиве или -1, если элемент не найден.
+    """
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        guess = arr[mid]
+
         if guess == target:
             return mid
-        elif guess>target:
-            high=mid-1
-
+        elif guess > target:
+            high = mid - 1
         else:
-            low = mid+1
+            low = mid + 1
 
     return -1
 
@@ -20,3 +29,4 @@ my_list = [1,3,5,7,9,11,13,15]
 target_value=7
 
 result = binary_search(my_list,target_value)
+print(result)
